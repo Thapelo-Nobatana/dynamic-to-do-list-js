3c667970006ea21cae6466cbf8ad476a7b8b8a73
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-08-01 21:42:10 +0200

    feat: build dynamic To-Do List app with add and remove task functionality

---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const addButton = document.getElementById("add-task-btn");
+    const taskInput = document.getElementById("task-input");
+    const taskList = document.getElementById("task-list");
+
+    // Function to add a new task
+    function addTask() {
+       const taskText = taskInput.value.trim();
+        if(taskText === ""){
+            alert("Please enter a task.");
+            return;
+        } else {
+            const listItem = document.createElement("li");
+            listItem.textContent = taskText;
+
+            const deleteButton = document.createElement("button");
+            deleteButton.textContent = "Remove";
+            deleteButton.setAttribute("class", "remove-btn");
+            deleteButton.addEventListener("click", function() {
+                taskList.removeChild(listItem);
+            });
+            listItem.appendChild(deleteButton);
+            taskList.appendChild(listItem);
+            taskInput.value = ""; // Clear the input field
+        }
+
+
+
+    }
+      addButton.addEventListener("click", addTask);
+     taskInput.addEventListener("keypress", function(event) {
+        if (event.key === "Enter") {
+            addTask();
+        }
+            });
+//   addTask(); // Initial call to set up event listeners
+});
